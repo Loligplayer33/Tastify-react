7de3b2183c3ee0bb444622f5435f9a4438985f1f
David Haase <dhaase033@gmail.com>
2020-04-26 18:14:41 +0200

    Display individual recipe - no ingredient info

--- src/components/Recipe/Recipe.js
+++ src/components/Recipe/Recipe.js
@@ -1,26 +1,85 @@
 import React, { useState, useEffect } from 'react';
 import getRecipe from '../Search/apiRecipe';
 
+import s from './recipe.module.scss';
+
 function Recipe({ id }) {
-  const [recipe, setRecipe] = useState({});
+  const [recipe, setRecipe] = useState(undefined);
+  const [servings, setServings] = useState(
+    recipe === undefined ? 6 : recipe.servings
+  );
 
   useEffect(() => {
     async function getApiResults() {
       const recipeData = await getRecipe(id);
-      setRecipe(recipeData);
+      // just set the recipe data - no config info needed
+      setRecipe(recipeData.data);
     }
     getApiResults();
+    console.log(recipe);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [id]);
 
-  console.log(recipe);
+  // TODO make sure that works tomorrow (to many requests for today)
+  if (recipe === undefined) {
+    return <h1>Loading...</h1>;
+  } else if (recipe !== undefined) {
+    return (
+      <div className={s.container}>
+        <header className={s.header}>
+          <div className={s.headerLead}>
+            <h1 className={s.heading}>
+              {/* hier z.b ist recipe.title noch undefined */}
+              {recipe.title.length > 50
+                ? recipe.title.slice(0, 50) + '...'
+                : recipe.title}
+            </h1>
+            <p className={s.categories}>
+              <span className={s.categoryColor}>categories: </span>
+              {recipe.cuisines.map(function renderCuisines(cuisine, index) {
+                if (index < 2) {
+                  return ` ${cuisine} / `;
+                } else if (index === 2) {
+                  return ` ${cuisine}`;
+                }
+              })}
+            </p>
+            <div className={s.imgContainer}>
+              <img className={s.img} src={recipe.image} alt="recipe" />
+            </div>
+          </div>
+        </header>
+        <aside className={s.recipeInfo}>
+          <div className={s.contentHolder}>
+            <h3 className={s.infoHeading}>Preparation</h3>
+            <p className={s.infoContent}>{recipe.preparationMinutes}</p>
+          </div>
 
-  return (
-    <div>
-      <h1>hi</h1>
-      <h1>hi</h1>
-      <h1>hi</h1>
-    </div>
-  );
+          <div className={s.contentHolder}>
+            <h3 className={s.infoHeading}>Cook</h3>
+            <p className={s.infoContent}>{recipe.readyInMinutes}</p>
+          </div>
+
+          <div className={s.contentHolder}>
+            <h3 className={s.infoHeading}>Servings</h3>
+            <input
+              className={`${s.servingsInput} ${s.infoContent}`}
+              type="text"
+              name="servings"
+              id="servings"
+              value={servings}
+              onChange={function setServingCount(e) {
+                setServings(e.target.value);
+              }}
+            />
+          </div>
+        </aside>
+        {/* <div className={s.recInfoContainer}>
+          <section className={s.ingredients}></section>
+        </div> */}
+      </div>
+    );
+  }
 }
 
 export default Recipe;
--- src/components/Search/apiRecipe.js
+++ src/components/Search/apiRecipe.js
@@ -1,6 +1,5 @@
 import axios from 'axios';
 
-// TODO changes prob necessary
 const BASE_URL = 'https://api.spoonacular.com/recipes';
 const apiKey = '5e5c37bdaec64ebb8bb2f7feb9b6ff27';
 
